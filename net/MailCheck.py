208d3acb0e2cebff5aad59a7f75197abb603453a
Leon Lambert <shi406406@qq.com>
2020-11-02 09:20:47 +0800

    update github hosts

--- net/MailCheck.py
+++ net/MailCheck.py
@@ -5,7 +5,7 @@ import re
 API = "https://mail.bjtu.edu.cn/coremail/xphone/index.jsp"
 head = {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
 reobj = re.compile(r"^(?P<no>\d+)\s+(?P<name>.+?)\s+.*\d{12}(?P<pwd>\d{5}[0-9X])")
-isResume = False
+isResume = True
 if __name__ == "__main__":
 
     for l in read_lines("mails.txt"):
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         pwd = match.groupdict()["pwd"]
 
         print("%s %s %s" % (no, name, pwd))
-        if no == "16121739":
+        if no == "16121174":
             isResume = True
 
         if not isResume:
