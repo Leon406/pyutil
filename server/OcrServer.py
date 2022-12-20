72784c4aea2e99136a33f112a7eb4a3339ece6f9
Leon Lambert <deadogone@gmail.com>
2022-12-20 17:32:24 +0800

    feat: 新增ocr验证码识别服务接口服务加入限制

--- server/OcrServer.py
+++ server/OcrServer.py
@@ -2,6 +2,7 @@ import ddddocr, time, json, threading, configparser, os
 from flask import Flask, request
 import platform
 import requests
+
 # ImportError: libGL.so 解决方案  https://www.cnblogs.com/mrneojeep/p/16252044.html
 # ubuntu
 # apt-get update && apt-get install libgl1
@@ -17,6 +18,12 @@ app = Flask(__name__)
 
 ddddocr_list = []
 ddddocr_state = []
+users = {}
+restrict_users = {}
+black_users = set()
+RATE_TIME = 120
+RATE_LIMIT = RATE_TIME / 10
+BLACK_OVER_LIMIT = RATE_LIMIT
 
 
 def init():
@@ -43,16 +50,55 @@ def destroy_ddddocr(i):
     return 0
 
 
+def check_limit(ip):
+    if ip in users:
+        user = users[ip]
+        print("current %s" % user)
+        if ip in black_users:
+            raise Exception("You request too much, and now in blacklist!!!")
+        if time.time() - user["time"] < RATE_TIME:
+            if user["count"] < RATE_LIMIT:
+                user["count"] += 1
+            else:
+                print("black ip %s" % ip)
+                if ip in restrict_users:
+                    restrict_users[ip] = restrict_users[ip] + 1
+                else:
+                    restrict_users[ip] = 1
+                print("black  %s" % restrict_users)
+                if restrict_users[ip] > BLACK_OVER_LIMIT:
+                    black_users.add(ip)
+                raise Exception("request limit!!! Try again in %s min" % int(RATE_TIME / 60))
+        else:
+            reset_limit(ip)
+    else:
+        reset_limit(ip)
+
+
+def reset_limit(ip):
+    users[ip] = {
+        "time": time.time(),
+        "count": 1,
+    }
+    if ip in restrict_users:
+        restrict_users.pop(ip)
+    print("users %s %s" % (ip, users))
+
+
 # 支持 参数 url, base64,file
 @app.route('/ocr', methods=['POST'])
 def ocr():
     try:
+        ip = request.remote_addr
+        if "X-Forwarded-For" in request.headers:
+            ip = request.headers.getlist("X-Forwarded-For")[0]
+        check_limit(ip)
         if "base64" in request.form:
             b64 = request.form['base64']
             b64 = b64[b64.find(',') + 1:]
-            return classify(b64)
+            return classify(b64, ip)
         if "url" in request.form:
-            return classify(requests.get(request.form['url']).content)
+            return classify(requests.get(request.form['url']).content, ip)
         file = request.files['file']
         if file:
             filename = file.filename
@@ -62,13 +108,35 @@ def ocr():
         else:
             return json.dumps({'code': False, 'msg': '服务器错误'})
 
-        return classify(file.read())
+        return classify(file.read(), ip)
     except Exception as e:
         print(e)
         return json.dumps({'status': False, 'msg': str(e)})
 
 
-def classify(content):
+@app.route('/unlock', methods=['GET'])
+def unlock():
+    ip = request.remote_addr
+    if "X-Forwarded-For" in request.headers:
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    print("unlock black %s" % restrict_users)
+    if ip in black_users:
+        black_users.remove(ip)
+    reset_limit(ip)
+    print("after unlock black %s" % restrict_users)
+    return json.dumps({'status': True, 'msg': "unlock success", 'ip': ip})
+
+
+@app.route('/user', methods=['GET'])
+def user():
+    return json.dumps({'status': True,
+                       'msg': "Success",
+                       'user': users,
+                       'restrict': restrict_users,
+                       'black': list(black_users)})
+
+
+def classify(content, ip):
     i = get_ddddocr()
     if i == -1:
         return json.dumps({'status': False, 'msg': '没有空闲的OCR线程'})
@@ -78,9 +146,10 @@ def classify(content):
     end = time.time()
     destroy_ddddocr(i)
     print("线程", i, "已释放")
-    return json.dumps({'status': True, 'msg': 'SUCCESS', 'result': data, 'usage': end - start})
+    return json.dumps({'status': True, 'msg': 'SUCCESS', 'result': data, 'usage': end - start, 'ip': ip,
+                       'remain': RATE_LIMIT - users[ip]["count"]})
 
 
 if __name__ == '__main__':
     threading.Thread(target=init).start()
-    app.run(host=service['listen'], port=service['port'], debug=False)
+    app.run(host=service['listen'], port=service['port'], debug=True)
