8d51d8e8d72c5cc61dc7e0fee052998daff649b3
Leon Lambert <deadogone@gmail.com>
2023-11-20 11:20:04 +0800

    fix(baidu): 文件数量大于1000的错误,优化请求参数和重试机制

--- baidu/BaiduNetdiskUnion.py
+++ baidu/BaiduNetdiskUnion.py
@@ -1,36 +1,41 @@
-import re
 import json
+import re
 import time
+
 import requests
 
 # 需要接入百度网盘SDK https://pan.baidu.com/union/doc/Jl0j9pza3
 ACCESS_TOKEN = ""
-SEARCH_KEY = "学习"
-DIR = "/"
-API_SEARCH = (f"http://pan.baidu.com/rest/2.0/xpan/file?dir={DIR}&access_token={ACCESS_TOKEN}"
-              f"&recursion=1&page=%s&method=search&key=%s")
-PAGE = 3
 
-# REGEXP = re.compile("_?【瑞客论坛 www\.ruike1\.com】|(瑞客论坛 www.ruike1.com)?\[瑞客论坛 www.ruike1.com]|\[瑞客论坛 www\.ruike1\.com]")
-REGEXP = re.compile("\[瑞客论坛 www.ruike1.com]")
+SEARCH_KEY = "瑞客论坛"
+DIR = "/ALIST/02. java/2023-黑马java V13.0"
+API_SEARCH = (
+    f"http://pan.baidu.com/rest/2.0/xpan/file?dir={DIR}&access_token={ACCESS_TOKEN}"
+    f"&recursion=1&page=%s&method=search&key=%s"
+)
+PAGE = 20
+
+REGEXP = re.compile(
+    "_?【瑞客论坛 www\.ruike1\.com】|(瑞客论坛 www.ruike1.com)?\[瑞客论坛 www.ruike1.com]|\[瑞客论坛 www\.ruike1\.com]"
+)
+# REGEXP = re.compile("\[瑞客论坛 www.ruike1.com]")
+# 自定义水印
 REPLACE = r""
-REG_DELETE = re.compile(r"\.url$")
-headers = {
-    'User-Agent': 'pan.baidu.com'
-}
+# REG_DELETE = re.compile(r"\.url$")
+REG_DELETE = re.compile(r"/target/(?:test-classes|maven-status|classes)")
+headers = {"User-Agent": "pan.baidu.com"}
 
-API_FILE_MANAGER = f'http://pan.baidu.com/rest/2.0/xpan/file?method=filemanager&access_token={ACCESS_TOKEN}&opera=%s'
+API_FILE_MANAGER = f"http://pan.baidu.com/rest/2.0/xpan/file?method=filemanager&access_token={ACCESS_TOKEN}&opera=%s"
 API_RENAME = API_FILE_MANAGER % "rename"
 API_DELETE = API_FILE_MANAGER % "delete"
 
 
 def search(key, page=1):
     url = API_SEARCH % (page, key)
-    # print(url)
     response = requests.get(url, headers=headers)
     jsonStr = response.json()
-    print(jsonStr)
-    if 'has_more' not in jsonStr:
+    # print(jsonStr)
+    if "has_more" not in jsonStr:
         return 0, [], []
 
     datas = jsonStr["list"]
@@ -38,36 +43,51 @@ def search(key, page=1):
     files = []
     dirs = []
     for data in datas:
-        print(data)
+        # print(data)
         if data["isdir"] == 1:
             dirs.append({"id": data["fs_id"], "path": data["path"]})
         else:
             files.append({"id": data["fs_id"], "path": data["path"]})
-    return jsonStr["has_more"], files, dirs
+    return int(jsonStr["has_more"]), files, dirs
 
 
 def rename(file_list):
+    """
+    https://pan.baidu.com/union/doc/mksg0s9l4
+    """
     try_max = 5
     try_count = 0
     params = {
-        'async': '2',
-        'ondup': 'fail',
-        'filelist': json.dumps(file_list, ensure_ascii=False),
+        # async 0 同步，1 自适应，2 异步
+        "async": 0,
+        "ondup": "fail",
+        "filelist": json.dumps(file_list, ensure_ascii=False),
     }
-    response = requests.post(API_RENAME,data=params, headers=headers)
+    response = requests.post(API_RENAME, data=params, headers=headers)
     response.raise_for_status()
-    errno = response.json()['errno']
+    errno = response.json()["errno"]
+    # print(response.json())
     if errno == 0:
-        print('[info] : rename successfully!')
+        print(f"[info] : rename successfully! {len(file_list)}")
     elif errno == 12:
-        print(response.json())
-        print('[warning]:批量处理错误，5s后重试 - 总数量:{}'.format(len(file_list)))
+        needRetry = False
+        for item in response.json()["info"]:
+            # -9	文件不存在 -7	文件名非法 111	有其他异步任务正在执行
+            if item["errno"] not in [-9, 111]:
+                needRetry = True
+            else:
+                pass
+                # print(item["errno"])
+        if not needRetry:
+            return
+
+        print("[warning]:批量处理错误，5s后重试 - 总数量:{}".format(len(file_list)))
         try_count += 1
         if try_count <= try_max:
             time.sleep(5)
             rename(file_list)
         else:
-            print('[error] : 批量处理错误且达到最大重试上限')
+            print("[error] : 批量处理错误且达到最大重试上限")
     else:
         print(response.json())
 
@@ -76,30 +96,41 @@ def delete(file_list):
     try_max = 3
     try_count = 0
     params = {
-        'async': '2',
-        'ondup': 'fail',
-        'filelist': json.dumps(file_list, ensure_ascii=False),
+        "async": 0,
+        "ondup": "fail",
+        "filelist": json.dumps(file_list, ensure_ascii=False),
     }
-    print(file_list)
+    # print(file_list)
     response = requests.post(API_DELETE, data=params, headers=headers)
     response.raise_for_status()
-    errno = response.json()['errno']
+    errno = response.json()["errno"]
     if errno == 0:
-        print('[info] : delete successfully!')
+        print(f"[info] : delete successfully! {len(file_list)}")
     elif errno == 12:
-        print('[warning]:批量处理错误，5s后重试 - 总数量:{}'.format(len(file_list)))
+        needRetry = False
+        for item in response.json()["info"]:
+            # -9	文件不存在 -7	文件名非法 111	有其他异步任务正在执行
+            if item["errno"] not in [-9, 111]:
+                needRetry = True
+            else:
+                pass
+                # print(item)
+        if not needRetry:
+            return
+
+        print("[warning]:批量处理错误，5s后重试 - 总数量:{}".format(len(file_list)))
         try_count += 1
         if try_count <= try_max:
             time.sleep(5)
-            delete(file_list)
+            rename(file_list)
         else:
-            print('[error] : 批量处理错误且达到最大重试上限')
+            print("[error] : 批量处理错误且达到最大重试上限")
     else:
         print(response.json())
     pass
 
 
-if __name__ == '__main__':
+def check():
     page = 1
     has_more, files, dirs = search(SEARCH_KEY, page)
     print(files)
@@ -112,24 +143,33 @@ if __name__ == '__main__':
         print(f"=========> has_more{has_more}")
         files.extend(tmp_files)
         dirs.extend(tmp_dirs)
-
     print("=====> 匹配文件", len(files))
     print("=====> 匹配文件夹", len(dirs))
     for file in files:
-        if REGEXP.search(file["path"]):
+        if REG_DELETE.search(file["path"]):
+            deletelist.append(file["path"])
+        elif REGEXP.search(file["path"]):
             name = file["path"][file["path"].rfind("/") + 1:]
             file["newname"] = REGEXP.sub(REPLACE, name)
             filelist.append(file)
-        if REG_DELETE.search(file["path"]):
-            deletelist.append(file["path"])
-
-    print("=====> 重命名文件", len(filelist), filelist)
+    fileListLen = len(filelist)
+    print("=====> 重命名文件", fileListLen, filelist)
     print("=====> 删除文件", deletelist)
-
-    if len(filelist) > 0:
-        print(filelist)
-        rename(filelist)
+    if fileListLen > 0:
+        # print(filelist)
+        iteration = (fileListLen // 1000) + 1
+        for i in range(0, iteration):
+            fr = 1000 * i
+            end = 1000 * (i + 1)
+            print(f"{fr} -> {end} {i}")
+            rename(filelist[fr: end])
     #
     if len(deletelist) > 0:
-        delete_data = {"filelist": json.dumps(deletelist, ensure_ascii=False)}
         delete(deletelist)
+
+
+
+if __name__ == "__main__":
+    for i in range(1, 100):
+        check()
+        time.sleep(60)
